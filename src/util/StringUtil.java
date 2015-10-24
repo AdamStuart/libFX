1fccbd10af2e55ca2b6ceca32bc0359dd36cfcf1
adamtreister <adamtreister@gmail.com>
2015-10-23 19:00:23 -0700

    snarfed getSuperscript method from waterloo util

--- src/util/StringUtil.java
+++ src/util/StringUtil.java
@@ -414,6 +414,23 @@ public class StringUtil
 		return "'" + content.replaceAll("'", "") + "'";	
 	}
 
+	public static String getSuperscript(char input) 
+	{
+		switch (input) 
+		{
+		    case '0':   return ("\u2070");
+		    case '1':   return ("\u00B9");
+		    case '2':   return ("\u00B2");
+		    case '3':   return ("\u00B3");
+		    case '4':   return ("\u2074");
+		    case '5':   return ("\u2075");
+		    case '6':   return ("\u2076");
+		    case '7':   return ("\u2077");
+		    case '8':   return ("\u2078");
+		    case '9':   return ("\u2079");
+		}
+		return "";
+	}
 
 
 }
