36c52476a31d09ea6078f3745d276a8f003f70ef
Adam Treister <adam.treister@gladstone.ucsf.edu>
2016-08-06 11:07:12 -0700

    add null constructor

--- src/model/Range.java
+++ src/model/Range.java
@@ -9,8 +9,9 @@ public class Range
 	public double width() { return max-min;	}
 	public void set(double mini, double maxi)	{ 	min = mini; max = maxi;	 }
 	
+	public Range()							{		this(0,1);	}
 	public Range(float mini, float maxi)	{		min = mini; max = maxi;	}
 	public Range(double mini, double maxi)	{		min = mini; max = maxi;	}
 	public Range(int mini, int maxi)		{		min = mini; max = maxi;	}
-	public String toString()				{ return "[" + min + " - " + max + "]"; }
+	public String toString()				{ 		return "[" + min + " - " + max + "]"; }
 }
